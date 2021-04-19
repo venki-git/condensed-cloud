9b3f0ef6206d67c89b8794e6c39ddc958ca270f8
venki-git <82790771+venki-git@users.noreply.github.com>
2021-04-19 16:18:51 +0530

    Update load_data.sql

--- bq-sql/load_data.sql
+++ bq-sql/load_data.sql
@@ -1,17 +1,17 @@
-CREATE TABLE Employee
+CREATE TABLE condensed_cloud.Employees
     ( 
-    FirstName Varchar(100), 
-    LastName Varchar(100), 
-    Country Varchar(100),
-	Salary INT  
+    FirstName STRING, 
+    LastName STRING, 
+    Country STRING,
+	Salary INT64  
     ) 
-    GO 
+
     
-Insert into Employee (FirstName,LastName,Country )values('Rachel','Green','India',25000),
+Insert into condensed_cloud.Employees (FirstName,LastName,Country,Salary )values('Rachel','Green','India',25000),
                                 ('Rachel','Green','India',25000),
                                 ('Phoebe','buffay','USA',28000),
                                 ('Ross','Geller','UK',35000),
                                 ('Ross','Geller','UK',35000),
                                 ('Joey','Tribioni','UK',35000),
-								('Monica','Geller','USA',50000),
-								('Chandler','Bing',45000)
+				('Monica','Geller','USA',50000),
+				('Chandler','Bing','India',45000)
